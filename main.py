a20e27201c61816f9355d1dfa285524591456b06
Arda Bozdere <admin@echothedeveloper.com>
2025-05-06 16:46:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-import sys  
+import sys
 
 num = input("Enter Card Number: ")
 
-if len(num) != 16:
+if len(num) != 16 or not num.isdigit():
     print("Card number is not a valid card number! Must be 16 digits!")
     sys.exit()
 
 r_num = num[::-1]
 total = 0
 
-for i in range(0, len(r_num)):
+for i in range(len(r_num)):
     digit = int(r_num[i])
-    if i % 2 == 0:
-        total = total + (digit * 2)
-    else:
-        total = total + int(digit)
+    if i % 2 == 1:
+        digit *= 2
+        if digit > 9:
+            digit -= 9
+    total += digit
 
-if total % 1 == 0:
+if total % 10 == 0:
     print("This is a valid card number")
 else:
     print("This is not a valid card number")
-
